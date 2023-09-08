845d8d6cb465e34cea529f01a8228cad02aeb8d3
Alen Scaria <107207581+Alen-Scaria1516@users.noreply.github.com>
2023-09-08 07:46:01 +0530

    Update left_factorial.c

--- left_factorial.c
+++ left_factorial.c
@@ -34,3 +34,92 @@
 34:       printf("\n A->%s",modifiedGram);  
 35:       printf("\n X->%s\n",newGram);  
 36:  }  
+/*
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_RULES 10
+#define MAX_RULE_LENGTH 10
+#define MAX_NON_TERMINALS 10
+#define MAX_TERMINALS 10
+char rules[MAX_RULES][MAX_RULE_LENGTH];
+char nonTerminals[MAX_NON_TERMINALS];
+char terminals[MAX_TERMINALS];
+char newRules[MAX_RULES][MAX_RULE_LENGTH];
+
+int numRules, numNonTerminals, numTerminals, numNewRules;
+
+void inputGrammar() {
+    printf("Enter the number of rules: ");
+    scanf("%d", &numRules);
+    getchar(); // Consume newline
+
+    printf("Enter the rules (e.g., A->ab|ac|b): ");
+    for (int i = 0; i < numRules; i++) {
+        fgets(rules[i], MAX_RULE_LENGTH, stdin);
+    }
+
+    printf("Enter the non-terminals: ");
+    fgets(nonTerminals, MAX_NON_TERMINALS, stdin);
+    numNonTerminals = strlen(nonTerminals) - 1;
+
+    printf("Enter the terminals: ");
+    fgets(terminals, MAX_TERMINALS, stdin);
+    numTerminals = strlen(terminals) - 1;
+}
+
+int findCommonPrefixLength(char* str1, char* str2) {
+    int i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0' && str1[i] == str2[i]) {
+        i++;
+    }
+    return i;
+}
+
+void eliminateLeftFactoring() {
+    numNewRules = 0;
+
+    for (int i = 0; i < numRules; i++) {
+        int commonPrefixLength = 0;
+
+        for (int j = i + 1; j < numRules; j++) {
+            commonPrefixLength = findCommonPrefixLength(rules[i] + 3, rules[j] + 3);
+
+            if (commonPrefixLength > 0) {
+                char newNonTerminal = nonTerminals[numNonTerminals];
+                nonTerminals[numNonTerminals++] = newNonTerminal
+sprintf(newRules[numNewRules++], "%c->%.*s%c'", rules[i][0], commonPrefixLength, rules[i] + 3, newNonTerminal);
+                sprintf(newRules[numNewRules++], "%c'->%s", newNonTerminal, rules[j] + 3 + commonPrefixLength);
+                sprintf(newRules[numNewRules++], "%c'->epsilon", newNonTerminal);
+
+                break;
+            }
+        }
+
+        if (commonPrefixLength == 0) {
+            strcpy(newRules[numNewRules++], rules[i]);
+        }
+    }
+}
+
+void displayGrammar(char grammar[][MAX_RULE_LENGTH], int numRules) {
+    for (int i = 0; i < numRules; i++) {
+        printf("%s", grammar[i]);
+    }
+}
+
+int main() {
+    inputGrammar();
+
+    printf("\nOriginal Grammar:\n");
+    displayGrammar(rules, numRules);
+
+    eliminateLeftFactoring();
+
+    printf("\nGrammar after eliminating left factoring:\n");
+    displayGrammar(newRules, numNewRules);
+
+    return 0;
+}
+
+*/
